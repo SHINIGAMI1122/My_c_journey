bc9443e73fd5fcc07a51aba17030955db2d99bfc
SHINIGAMI1122 <aman1234gaur@gmail.com>
2026-03-26 01:40:46 +0530

    notes

---
+++ 26.03.2026/eg39.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    x=100;
+    y=234543;
+    printf("%d\n",x);
+    printf("%d\n",y);
+    printf("\n\n");
+    printf("%12d\n",x);
+    printf("%12d\n",y);
+    return 0;
+}
